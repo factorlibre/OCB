d3f192ef3494c7b1f6d381cb972c1c5feb9f85d7
Hugo Santos <hugo.santos@factorlibre.com>
2016-03-16 16:53:39 +0100

    [IMP] Improved performance when compute invoice amount_untaxed

--- addons/account/account_invoice.py
+++ addons/account/account_invoice.py
@@ -63,7 +63,12 @@ class account_invoice(models.Model):
     @api.one
     @api.depends('invoice_line.price_subtotal', 'tax_line.amount')
     def _compute_amount(self):
-        self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line)
+        self.env.cr.execute("""SELECT COALESCE(SUM(price_subtotal), 0.0)
+            FROM account_invoice_line
+            WHERE invoice_id=%s""", (self.id,))
+        sql_res = self.env.cr.fetchone()
+        amount_untaxed = sql_res[0]
+        self.amount_untaxed = amount_untaxed
         self.amount_tax = sum(line.amount for line in self.tax_line)
         self.amount_total = self.amount_untaxed + self.amount_tax
 
